d6f8a1d40c123a25379a03a3cdc56b981b594377
rjgj <jaictinrj@gmail.com>
2021-02-17 14:10:34 +0800

    added first_name and last_name to user creation and implemented the default group to be employees

--- tasker_site/tasker/views.py
+++ tasker_site/tasker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 
 from .forms import CreateUserForm
 from .decorators import unauthenticated_user, allowed_users
@@ -18,8 +19,13 @@ def register(request):
         form = CreateUserForm(request.POST)
 
         if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
+            user = form.save()
+
+            # default group for new user to employees
+            group = Group.objects.get(name='employees')
+            user.groups.add(group)
+
+            # user = form.cleaned_data.get('username')
             messages.success(request, f'Account was registered for {user}!')
 
             return redirect('login')
