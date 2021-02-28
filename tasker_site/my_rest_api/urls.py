767dd998a757f055d5a2fe6fb41892de738f3549
rjgj <jaictinrj@gmail.com>
2021-02-28 19:02:20 +0800

    Implemented REST API on USER Model

--- tasker_site/my_rest_api/urls.py
+++ tasker_site/my_rest_api/urls.py
@@ -5,14 +5,26 @@ from .views import (
     task_detail,
     task_create,
     task_update,
-    task_delete
+    task_delete,
+    task_subtasks,
+    user_list,
+    user_detail,
+    user_update,
 )
 
 urlpatterns = [
     path('', api_overview, name='api-overview'),
+
+    # Task
     path('task-list/', task_list, name='task-list'),
     path('task-detail/<str:pk>/', task_detail, name='task-detail'),
     path('task-create/', task_create, name='task-create'),
     path('task-update/<str:pk>/', task_update, name='task-update'),
     path('task-delete/<str:pk>/', task_delete, name='task-delete'),
+    path('task-subtasks/<str:pk>/', task_subtasks, name='task-subtasks'),
+
+    # User
+    path('user-list/', user_list, name='user-list'),
+    path('user-detail/<str:pk>/', user_detail, name='user-detail'),
+    path('user-update/<str:pk>/', user_update, name='user-update'),
 ]
--- tasker_site/my_rest_api/views.py
+++ tasker_site/my_rest_api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import TaskSerializer
+from .serializers import TaskSerializer, UserSerializer
 from tasker.models import Task
 
 
@@ -12,11 +13,19 @@ from tasker.models import Task
 def api_overview(request):
 
     api_urls = {
-        'List': '/task-list/',
-        'Detail View': '/task-detail/<str:pk>',
-        'Create': '/task-create/',
-        'Update': '/task-update/<str:pk>',
-        'Delete': '/task-delete/<str:pk>',
+
+        # Task
+        'Task List': '/task-list/',
+        'Task Detail View': '/task-detail/<str:pk>',
+        'Task Create': '/task-create/',
+        'Task Update': '/task-update/<str:pk>',
+        'Task Delete': '/task-delete/<str:pk>',
+        'Task List Subtasks': '/task-subtasks/<str:pk>',
+
+        # User
+        'User List': '/user-list/',
+        'User Detail View': '/user-detail/<str:pk>',
+        'User Update': '/user-update/<str:pk>',
     }
     return Response(api_urls)
 
@@ -66,3 +75,40 @@ def task_delete(request, pk):
     task.delete()
 
     return Response('Item successfully deteted')
+
+
+@api_view(['GET'])
+def task_subtasks(request, pk):
+
+    tasks = Task.objects.filter(parent=pk)
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+# User
+@api_view(['GET'])
+def user_list(request):
+
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def user_detail(request, pk):
+
+    users = User.objects.get(id=pk)
+    serializer = UserSerializer(users, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def user_update(request, pk):
+
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(instance=user, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
