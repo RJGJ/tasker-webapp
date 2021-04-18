b77e846e25ee59ff0b2b1a64ddb0fd0374e768d4
RJGJ <jaictinrj@gmail.com>
2021-04-18 09:48:37 +0800

    initial commit in dev-with-gui branch

--- tasker_site/tasker_frontend/urls.py
+++ tasker_site/tasker_frontend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
-from .views import register
+from .views import index
 
 urlpatterns = [
-	path('register/', register, name='register')
+	# path('register/', register, name='register'),
+	path('', index, name="index"),
 ]
